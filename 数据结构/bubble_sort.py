6e9ab072e6c64a6a64ff374d391322153ac8fd46
shadowimp <1462168028@qq.com>
2018-04-12 11:30:44 +0800

    Update bubble_sort.py
    add some explanation

--- 数据结构/bubble_sort.py
+++ 数据结构/bubble_sort.py
@@ -1,6 +1,6 @@
 def bubble_sort(nums): 
-	for i in range(len(nums)-1):
-		for j in range(len(nums)-1-i):
+	for i in range(len(nums)-1): #并不需要n趟， 只需要n-1趟。 只剩一个元素的时候可以直接返回
+		for j in range(len(nums)-1-i):  #注意不要取nums[j+1]时下标不要超出范围
 			if nums[j] > nums[j+1]:
 				nums[j],nums[j+1] = nums[j+1],nums[j]
 		print(nums)
