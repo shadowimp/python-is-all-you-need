685d50ac890e2d0cdc5ef43eb2b1a36af80b6f90
shaowimp <1462168028@qq.com>
2018-04-12 12:13:27 +0800

    将归并排序的函数模块化

--- 数据结构/merge_sort.py
+++ 数据结构/merge_sort.py
@@ -1,31 +1,29 @@
-#-*-coding:utf-8-*-
 def merge_sort(nums):
-	if len(nums) >1 :  #如果数组只有一个元素或者没有元素，最后就会return数组本身
-		mid = len(nums)//2 
-		left = merge_sort(nums[:mid])   # 将数组拆分 ， 递归使函数从最底部一个元素的地方进行操作
-		right = merge_sort(nums[mid:]) 
+    if len(nums) <2: return nums
 
-		i,j,k = 0,0,0 	
-		while i<len(left) and j<len(right):  #left 和right 作比较 将较大元素给nums
-			if left[i] < right[j]: 
-				nums[k] = left[i] 
-				i = i+1 
-			else: 
-				nums[k] = right[j] 
-				j = j+1 
-			k = k+1 
+    mid = len(nums)//2
+    left = merge_sort(nums[:mid])  # 将数组不断地拆分，递归使函数从最底部一个元素的时候进行操作
+    right = merge_sort(nums[mid:])
 
-		while i<len(left):    # 如果最后有剩余的元素，将其直接附加在nums的后面
-			nums[k] = left[i]
-			i = i+1
-			k = k+1 
-		while j <len(right):
-			nums[k] = right[j] 
-			j = j+1 
-			k = k+1
-	return nums
+    return merge(left,right)
 
-a = [3,5,1,8,2,9] 
-b = [1]
-print(merge_sort(a))
-print(merge_sort(b))
+def merge(nums1,nums2):
+    """
+    合并两个有序的数组，产生一个新的有序数组
+    """
+    nums3 = []
+    i ,j = 0,0
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] < nums2[j]:
+            nums3.append(nums1[i])
+            i += 1
+        else:
+            nums3.append(nums2[j])
+            j += 1
+    nums3 += nums1[i:] #多余的元素直接加到结果的后面
+    nums3 += nums2[j:]
+    return nums3
+
+
+print(merge_sort([8,2,3,5,7,6,1,4]))
+
